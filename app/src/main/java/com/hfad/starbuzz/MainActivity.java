e724db7b9d2ea1008f6551e2f2866b0af1677fb9
quangcanh2975 <ubuntultc@gmail.com>
2020-08-29 10:39:02 +0700

    Added ListView to DrinkCategoryActivity, Set Image, Name and Description for Drinks

---
+++ app/src/main/java/com/hfad/starbuzz/MainActivity.java
@@ -0,0 +1,31 @@
+package com.hfad.starbuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // create onItemClick listener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        ListView listView = (ListView)findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
